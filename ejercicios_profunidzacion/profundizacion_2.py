6e3fa0d86299392ecf1d911ea062e07587d6cc88
Juanchamena <juanchamena@gmail.com>
2022-03-19 21:26:14 -0300

    Segundo ejercicio de profundizacion.

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -33,3 +33,18 @@ Finalmente el programa debe imprimir dos líneas de texto por separado
 
 print('Sistema de ingreso de datos')
 # Empezar aquí la resolución del ejercicio
+print("Ingrese su nombre completo:")
+nombre = str(input())
+print("Ingrese su DNI:")
+dni = int(input())
+print("Ingrese su edad:")
+edad = int(input())
+print("Ingrese su altura:")
+altura = float(input())
+
+print("Nombre completo:", nombre + ",", "DNI:", dni)
+print("Nombre completo:", nombre + ",", "Edad:", edad, "Altura:", altura)
+
+
+
+
